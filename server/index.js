2ed147ea2dc868b940e4ab0aa8bc0c030b670b13
Nasyih Ulwan <muhnasyihulwan@gmail.com>
2023-02-05 22:33:46 +0700

    add routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+// IMPORT FROM PACKAGES
 const express = require("express");
-const port = 3000;
 
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+// INIT
+const port = 3000;
 const app = express();
 
-app.listen(port, "0.0.0.0", function () {
+// MIDDLEWARE
+app.use(authRouter);
+
+app.listen(port, () => {
   console.log("connected at port " + port);
 });
